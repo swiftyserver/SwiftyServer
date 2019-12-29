bc04c34b820b36b6eb833e1fc6574b9b88e70d95
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2019-12-28 21:36:29 -0500

    Renamed project

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "serverTest",
+    name: "SwiftyServer",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -13,8 +13,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "serverTest",
-            dependencies: ["NIO", "NIOHTTP1", "NIOConcurrencyHelpers"])
+        .target(name: "SwiftyServer", dependencies: ["NIO", "NIOHTTP1", "NIOConcurrencyHelpers"])
     ]
 )
